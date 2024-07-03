e3a8e505f243ea4bcf47a2bc96cabe21d248ca33
cpmachado <cpmachado@protonmail.com>
2024-07-03 22:02:54 +0100

    Add database support for Spring Boot project - Step 3

--- kotlinlang/jvm-get-started-spring-boot/src/main/kotlin/com/example/demo/DemoApplication.kt
+++ kotlinlang/jvm-get-started-spring-boot/src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -2,8 +2,11 @@ package com.example.demo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.jdbc.core.query
+import org.springframework.stereotype.Service
+import org.springframework.web.bind.annotation.*
+import java.util.*
 
 @SpringBootApplication
 class DemoApplication
@@ -13,13 +16,37 @@ fun main(args: Array<String>) {
 }
 
 @RestController
-class MessageController {
+class MessageController(val service: MessageService) {
     @GetMapping("/")
-    fun index() = listOf(
-        Message("1", "Hello!"),
-        Message("2", "Bonjour!"),
-        Message("3", "Privet!"),
-    )
+    fun index() = service.findMessages()
+
+    @GetMapping("/{id}")
+    fun index(@PathVariable id: String): List<Message> =
+        service.findMessageById(id)
+
+    @PostMapping("/")
+    fun post(@RequestBody message: Message) {
+        service.save(message)
+    }
 }
 
 data class Message(val id: String?, val text: String)
+
+@Service
+class MessageService(val db: JdbcTemplate) {
+    fun findMessages(): List<Message> = db.query("SELECT * FROM messages") { response, _ ->
+        Message(response.getString("id"), response.getString("text"))
+    }
+
+    fun findMessageById(id: String): List<Message> =
+        db.query("SELECT * FROM messages WHERE id = ?", id) { response, _ ->
+            Message(response.getString("id"), response.getString("text"))
+        }
+
+    fun save(message: Message) {
+        val id = message.id ?: UUID.randomUUID().toString()
+        db.update(
+            "INSERT INTO messages VALUES (?, ?)", id, message.text
+        )
+    }
+}
