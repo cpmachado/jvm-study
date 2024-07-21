1e311a012982ca52bd8d12a29edd5efc753211f6
cpmachado <cpmachado@protonmail.com>
2024-07-22 00:37:59 +0100

    exercism/kotlin: acronym (improvements - 2024/07/22 0038)

--- exercism/kotlin/acronym/src/main/kotlin/Acronym.kt
+++ exercism/kotlin/acronym/src/main/kotlin/Acronym.kt
@@ -1,9 +1,13 @@
 object Acronym {
     fun generate(phrase: String): String {
         val sep = "[ \\-]".toRegex();
-        return phrase.split(regex = sep).map { it.replace("_", "") }.filter(String::isNotEmpty)
-            .map {
-                it.subSequence(0, 1).toString().uppercase()
-            }.joinToString(prefix = "", separator = "", postfix = "")
+        return phrase.split(regex = sep)
+            .map { it.replace("_", "") }
+            .filter(String::isNotEmpty)
+            .map { it[0].toString() }
+            .map(
+                String::uppercase
+            )
+            .joinToString("")
     }
 }
