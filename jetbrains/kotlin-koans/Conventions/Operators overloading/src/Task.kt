41109ee543c7ca8866e5d74007b5b835a46688b6
cpmachado <cpmachado@protonmail.com>
2024-07-22 20:55:21 +0100

    jetbrains/kotlin-koans: Conventions/Operators overloading

--- jetbrains/kotlin-koans/Conventions/Operators overloading/src/Task.kt
+++ jetbrains/kotlin-koans/Conventions/Operators overloading/src/Task.kt
@@ -5,12 +5,19 @@ data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
 // Supported intervals that might be added to dates:
 enum class TimeInterval { DAY, WEEK, YEAR }
 
-operator fun MyDate.plus(timeInterval: TimeInterval): MyDate = TODO()
+operator fun MyDate.plus(timeInterval: TimeInterval): MyDate = this.addTimeIntervals(timeInterval, 1)
+
+data class MultipleTimeInterval(val timeInterval: TimeInterval, val amount: Int)
+
+operator fun TimeInterval.times(n: Int): MultipleTimeInterval = MultipleTimeInterval(this, n)
+
+operator fun MyDate.plus(multipleTimeInterval: MultipleTimeInterval): MyDate =
+    this.addTimeIntervals(multipleTimeInterval.timeInterval, multipleTimeInterval.amount)
 
 fun task1(today: MyDate): MyDate {
     return today + YEAR + WEEK
 }
 
 fun task2(today: MyDate): MyDate {
-    TODO("Uncomment") //return today + YEAR * 2 + WEEK * 3 + DAY * 5
+    return today + YEAR * 2 + WEEK * 3 + DAY * 5
 }
